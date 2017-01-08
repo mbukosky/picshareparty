6c88ebeeaafa58e48151591472fa00433065de6b
Michael Bukosky <mbukosky@gmail.com>
2017-01-08 14:34:43 -0500

    Replaced sample file with real pic share party request
    - added mkdirp

--- index.js
+++ index.js
@@ -4,18 +4,39 @@ const _ = require('lodash');
 const Dropbox = require('dropbox');
 const wget = require('node-wget');
 const async = require('async');
+const mkdirp = require('mkdirp');
 
 const config = require('./config');
 const dbx = new Dropbox({
     accessToken: config.DROPBOX_TOKEN
 });
 
-// TODO: Load from request
-const $ = cheerio.load(fs.readFileSync('pcp-sample.html'))
+// TOOD: Make this a param
+var psp_tiny_url = 'http://bit.ly/2hRhWtC';
+var folder = _.trimStart(psp_tiny_url, 'http://bit.ly/');
 
 // Main entry point
 async.auto({
-    parse_images: function(callback) {
+    url: async.constant(psp_tiny_url),
+    source_path: async.constant('./' + folder + '/source.html'),
+    create_folder: async.apply(mkdirp, folder),
+    get_picsshareparty_page: ['url', 'source_path', 'create_folder', (args, callback) => {
+        wget({
+                url: args.url,
+                dest: args.source_path
+            },
+            (err, res, body) => {
+                if (err) {
+                    callback(err);
+                } else {
+                    callback(null, cheerio.load(fs.readFileSync(args.source_path)));
+                }
+            }
+        );
+    }],
+    parse_images: ['get_picsshareparty_page', (results, callback) => {
+        var $ = results.get_picsshareparty_page;
+
         // TODO: Better protection around refs
         var images = [];
         $("img").each(function(i, elem) {
@@ -25,24 +46,28 @@ async.auto({
             }
         });
 
-        // Remove any dups
-        callback(null, _.uniq(images));
-    },
-    get_images: ['parse_images', function(results, callback) {
+        // Delete source and remove any dups
+        fs.unlink(results.source_path, (err) => callback(err, _.uniq(images)));
+    }],
+    get_images: ['parse_images', (results, callback) => {
         async.map(results.parse_images, getImage, callback);
     }],
-    upload_images: ['get_images', function(results, callback) {
+    upload_images: ['get_images', (results, callback) => {
         // Getting a 429 rate limit with map
         async.mapSeries(results.get_images, uploadImage, callback);
+    }],
+    delete_folder: ['upload_images', (results, callback) => {
+        fs.rmdir(folder, callback);
     }]
-}, function(err, results) {
+}, (err, results) => {
     console.error('err = ', err);
     console.log('results = ', results);
 });
 
 function getImage(ref, callback) {
     wget({
-            url: ref
+            url: ref,
+            dest: './' + folder + '/'
         },
         callback
     );
@@ -54,10 +79,10 @@ var count = 0;
 function uploadImage(image, callback) {
     fs.readFile(image.filepath, (err, contents) => {
         if (err) return callback(err);
-
+        //TODO: replace with string util
         var filename = _.split(image.headers['content-disposition'],  '"',  2)[1];
         dbx.filesUpload({
-                path: '/' + (count++) + '-' + filename,
+                path: '/' + folder + '/' + (count++) + '-' + filename,
                 contents: contents
             })
             .then((res) => fs.unlink(image.filepath, (err) => callback(err, res)))
